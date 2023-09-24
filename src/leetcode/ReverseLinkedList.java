a73ad3925c049a72653e7f1a91178af2103a5093
sskim950105 <sskim950105@gmail.com>
2023-09-24 23:05:00 +0900

    LeetCode - Reverse Linked List

---
+++ src/leetcode/ReverseLinkedList.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+// https://leetcode.com/problems/reverse-linked-list/
+public class ReverseLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+        ListNode reversedNode = reverseList(node1);
+        while (reversedNode != null) {
+            System.out.println(reversedNode.val);
+            reversedNode = reversedNode.next;
+        }
+    }
+}
