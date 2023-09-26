0714a7cd270fd0723b572b69fad0772be9a2ec1b
sskim950105 <sskim950105@gmail.com>
2023-09-26 23:16:14 +0900

    LeetCode - Maximum Subarray

---
+++ src/leetcode/MaximumSubarray.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+// https://leetcode.com/problems/maximum-subarray/
+public class MaximumSubarray {
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int tmpSum = 0;
+            for (int j = i; j < nums.length; j++) {
+                tmpSum += nums[j];
+                maxSum = Math.max(maxSum, tmpSum);
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int output1 = maxSubArray(nums1); // 6
+        System.out.println("output1 = " + output1);
+
+        int[] nums2 = {1};
+        int output2 = maxSubArray(nums2); // 1
+        System.out.println("output2 = " + output2);
+
+        int[] num3 = {5, 4, -1, 7, 8};
+        int output3 = maxSubArray(num3); // 23
+        System.out.println("output3 = " + output3);
+    }
+}
